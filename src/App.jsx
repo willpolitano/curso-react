894928d8d241dc5e38672328213394365560f6d0
willpolitano <willianpolitano@gmail.com>
2021-07-31 10:14:42 -0300

    Passando funções por props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 import Dados from './Componentes/Dados'
 
 const App = () => {
+
+    const nm = () => {
+        return 'Willian Politano Fernandes'
+    } 
+
+    const idd = '22'
+    const obj = 'Dominar React'
+
+    const soma = (v1, v2) => {
+        return v1 + v2
+    }
+
     return  (
         <Dados 
-            nome='William Politano Fernandes' 
-            idade='22'
-            objetivo='Dominar React'
+            nome = {nm} 
+            idade = {idd}
+            objetivo = {obj}
+            sm = {soma}
         />
     )
 }
--- src/Componentes/Dados.jsx
+++ src/Componentes/Dados.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Dados = (props) => {
+    const n1 = 10
+    const n2 = 20
+
     return (
         <>
-            <p>Nome: {props.nome}</p>
+            <p>Nome: {props.nome()}</p>
             <p>Idade: {props.idade}</p>
             <p>Objetivo: {props.objetivo}</p>
+            <p>A soma de {n1} com {n2} é igual a {props.sm(n1, n2)}</p>
         </>
     )
 }
