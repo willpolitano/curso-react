b33fe71f366355d9f90e5a50ee86fe6e1c23a432
willpolitano <willianpolitano@gmail.com>
2021-08-03 21:03:25 -0300

    handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './index.css'
 
 const App = () => {
 
-const carros = ['Argo', 'Fiat', 'Megane']
-const listaCarros = carros.map((c,i) => <li key={i}>{i} - {c}</li>)
+const [nome, setNome] = useState('')
+const [carro, setCarro] = useState('PORSHE')
 
-    return (
-        <>
-            {listaCarros}
-        </>
-    )
+const handleChangeCarro = (e) => {
+    setNome(e.target.value)
+}
+
+return (
+    <>
+        <input 
+            type="text"
+            value={nome}
+            onChange={(e) => handleChangeCarro(e)} 
+        />
+
+        <p><strong>Nome: </strong> {nome}</p>
+
+        <br />
+        <br />
+        <br />
+
+        <select value={carro} onChange={(e) => setCarro(e.target.value)}>
+            <option value="HRV">HRV</option>
+            <option value="PORSHE">PORSHE</option>
+            <option value="CAMARO">CAMARO</option>
+        </select>
+
+        <p><strong>Carro: </strong> {carro}</p>
+
+    </>
+)
 
 }
 
